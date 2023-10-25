80fcf0e2d93d74c6c0b73712ea459851dbfceedc
LeanViko <leangant@gmail.com>
2023-10-25 19:54:41 -0300

    sonido mordida y evitar que la manzana aparezca en el cuerpo de la vibora

--- Snake.py
+++ Snake.py
@@ -25,6 +25,9 @@ class SNAKE:
                 self.body_br = pygame.image.load('Graphics/body_br.png').convert_alpha()
                 self.body_bl = pygame.image.load('Graphics/body_bl.png').convert_alpha()
 
+                #sonidos
+                self.crunch_sounds = pygame.mixer.Sound('Sound/crunch.wav')
+
 
         def draw_snake(self):
 
@@ -102,6 +105,9 @@ class SNAKE:
         def add_block(self):
                 self.new_block = True
         
+        def play_crunch_sound(self):
+                self.crunch_sounds.play()
+        
         
 
 
@@ -139,6 +145,11 @@ class MAIN:
                 if self.fruit.pos == self.snake.body[0]:#coincide la cabeza con la posicion de la fruta
                         self.fruit.randomize() #reposicionamiento de la fruta
                         self.snake.add_block()
+                        self.snake.play_crunch_sound()
+                
+                for block in self.snake.body[1:]:
+                        if block == self.fruit.pos:
+                                self.fruit.randomize()
         
         def check_fail(self): 
                 #impacto laterales
@@ -175,8 +186,16 @@ class MAIN:
                 score_x = int(cell_size*cell_number - 60)
                 score_y = int(cell_size*cell_number - 40)
                 score_rect = score_surface.get_rect(center = (score_x,score_y))
-                screen.blit(score_surface, score_rect)
+                apple_rect = apple.get_rect(midright = (score_rect.left,score_rect.centery))
 
+                backgr_rect = pygame.Rect(apple_rect.left,apple_rect.top,apple_rect.width + score_rect.width + 9,apple_rect.height)
+
+                pygame.draw.rect(screen,(167,209,200),backgr_rect)
+                screen.blit(score_surface, score_rect)
+                screen.blit(apple, apple_rect)
+                pygame.draw.rect(screen,(56,74,12),backgr_rect,2)
+
+pygame.mixer.pre_init(44100,-16,2,512)
 pygame.init()
 cell_size= 40
 cell_number=20
